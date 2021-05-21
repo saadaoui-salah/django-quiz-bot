19fa3ac3b920f5033459e648cbbdee4d8b8ff066
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-21 08:06:56 -0700

    cretae get method

--- quiz/views.py
+++ quiz/views.py
@@ -1,2 +1,10 @@
 from django.shortcuts import render
 from .serializers import RandomQuizSerializer
+from rest_framework.views import APIView
+from .models import Quiz
+
+class RandomQuiz(APIView):
+    def get(self, request, formate=None, **kwagrs):
+        question = Quiz.objects.filter().order_by('?')[:1]
+        serializer = RandomQuizSerializer(question)
+        
