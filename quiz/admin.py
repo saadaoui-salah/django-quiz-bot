7a04a9ac0539e4285315d8b0ae8202687d989554
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-21 07:34:26 -0700

    costomize display fields

--- quiz/admin.py
+++ quiz/admin.py
@@ -3,3 +3,10 @@ from .models import Quiz, Answers
 
 admin.site.register(Quiz)
 admin.site.register(Answers)
+
+class AnswersAdmin(admin.ModelAdmin):
+    list_display = (
+        'answers',
+        'is_correct', 
+        'questions'
+    )
