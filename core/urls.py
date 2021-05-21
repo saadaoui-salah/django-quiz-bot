48ff78155c7481e40546e582f6d263aa3559aa72
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-21 08:21:31 -0700

    add api url

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from quiz.views import RandomQuiz
 urlpatterns = [
     path('admin/', admin.site.urls),
-    include("quiz.urls")    
+    path("api/random/", RandomQuiz.as_view())    
 ]
