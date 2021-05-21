8cdd9a355b79d9deb387a18dce0f04afde9d1d97
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-21 07:18:15 -0700

    createv Quiz model

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Quiz(models.Model):
+
+    LEVEL = (
+        (0,'Any'),
+        (1,'Beginner'),
+        (2,'Intermediate'),
+        (3,'Advanced'),
+        (4,'Expert'),
+    )
+
+    title = models.CharField(max_length=300)
+    points =  models.SmallIntegerField()
+    difficulty = models.IntegerField(choices=LEVEL, default=0)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now=False,auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True,auto_now_add=False)
