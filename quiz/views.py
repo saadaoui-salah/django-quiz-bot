4d56cf079486982582840ff820846f12b425280d
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-21 08:09:25 -0700

    return data

--- quiz/views.py
+++ quiz/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .serializers import RandomQuizSerializer
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Quiz
 
 class RandomQuiz(APIView):
     def get(self, request, formate=None, **kwagrs):
         question = Quiz.objects.filter().order_by('?')[:1]
-        serializer = RandomQuizSerializer(question)
-        
+        serializer = RandomQuizSerializer(question, many=True)
+        return Response(serializer.data)
+
