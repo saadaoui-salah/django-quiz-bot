b7f7797f00889eb0f41e98040e1eb0bf79debc4a
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-24 11:24:47 -0700

    add get data from the api and convert it to json

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
 import discord 
+import requests
+import json
 
 client = discord.Client()
+
+def get_quesiotns():
+    respose = requests.get("")
+    json_data = json.loads(respose)
 
 @client.event
 async def on_message(message):
