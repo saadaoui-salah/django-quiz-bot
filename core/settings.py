030ec7fe0726844f7bb635d6d930f6d6013b4cb5
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-25 10:09:03 -0700

    add media config

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
