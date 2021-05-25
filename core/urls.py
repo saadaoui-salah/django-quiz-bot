ece63583cacc237f9265f73c310a8046498ee6c8
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-25 07:51:11 -0700

    add static urls

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from quiz.views import RandomQuiz
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/random/", RandomQuiz.as_view())    
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
