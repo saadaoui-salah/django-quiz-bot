c04e38b4334cc769609d68946dc554b66f9e11ad
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-25 10:11:26 -0700

    Delete static files dir

--- core/settings.py
+++ core/settings.py
@@ -121,9 +121,5 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
