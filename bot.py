7677049a38639702a28ea1d92a70282d1b40410a
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-25 09:12:43 -0700

    update waitng answer time to 2

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ async def on_message(message):
             return m.author == message.author and m.content.isdigit()
         
         try:
-            guess = await client.wait_for('message', check=check, timeout=5.0)
+            guess = await client.wait_for('message', check=check, timeout=2)
         except asyncio.TimeoutError:
             return await message.channel.send("Sorry, you took too long")
         
